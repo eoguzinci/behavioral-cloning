d6763b80756624ae13ce9ef9021bfbd977179b6c
eoguzinci <eoguzinci@gmail.com>
2017-04-01 23:32:20 +0200

    images flipped

--- clone.py
+++ clone.py
@@ -1,9 +1,13 @@
 import csv
 import cv2
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -12,24 +16,37 @@ images = []
 measurements = []
 for line in lines:
 	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = '../data/IMG' + filename
+	filename = source_path.split('\\')[-1]
+	current_path = '.\\data\\IMG\\' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
 	measurement = float(line[3]) # steering measurement
 	measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurement)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement*-1)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
+X_train = np.array(images)
+y_train = np.array(measurements)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+print(model.output_shape)
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch =7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
