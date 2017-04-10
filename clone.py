70e23870e3fd76e6de3307091995699f747fe209
eoguzinci <eoguzinci@gmail.com>
2017-04-11 00:13:17 +0200

    not working still

--- clone.py
+++ clone.py
@@ -1,10 +1,38 @@
 import csv
 import cv2
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional import Convolution2D
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers.convolutional import Conv2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
+
+import matplotlib.pyplot as plt
+
+
+
+# with open(csv_file, 'r') as f:
+#         reader = csv.reader(f)
+#         for row in reader:
+#             steering_center = float(row[3])
+
+#             # create adjusted steering measurements for the side camera images
+#             correction = 0.2 # this is a parameter to tune
+#             steering_left = steering_center + correction
+#             steering_right = steering_center - correction
+
+#             # read in images from center, left and right cameras
+#             directory = "..." # fill in the path to your training IMG directory
+#             img_center = process_image(np.asarray(Image.open(path + row[0])))
+#             img_left = process_image(np.asarray(Image.open(path + row[1])))
+#             img_right = process_image(np.asarray(Image.open(path + row[2])))
+
+#             # add images and angles to data set
+#             car_images.extend(img_center, img_left, img_right)
+#             steering_angles.extend(steering_center, steering_left, steering_right)
+
+batch_size = 128
+num_classes = 10
+epochs = 12
 
 lines = []
 with open('./data/driving_log.csv') as csvfile:
@@ -15,13 +43,14 @@ with open('./data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-	source_path = line[0]
-	filename = source_path.split('\\')[-1]
-	current_path = '.\\data\\IMG\\' + filename
-	image = cv2.imread(current_path)
-	images.append(image)
-	measurement = float(line[3]) # steering measurement
-	measurements.append(measurement)
+	for i in range(3):
+		source_path = line[i]
+		filename = source_path.split('\\')[-1]
+		current_path = '.\\data\\IMG\\' + filename
+		image = cv2.imread(current_path)
+		images.append(image)
+		measurement = float(line[3]) # steering measurement
+		measurements.append(measurement)
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -30,23 +59,58 @@ for image, measurement in zip(images, measurements):
 	augmented_images.append(cv2.flip(image,1))
 	augmented_measurements.append(measurement*-1)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+
+# img_rows, img_cols = X_train[0].shape[0], X_train[0].shape[1]
+# X_train = X_train.reshape(X_train.shape[0], img_cols, img_rows, 3)
+# X_test = X_test.reshape(X_test.shape[0], img_cols, img_rows, 1)
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 print(model.output_shape)
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
+model.add(Conv2D(24, (5, 5), data_format="channels_last", activation="relu", strides=(2, 2)))
+model.add(Conv2D(36, (5, 5), data_format="channels_last", activation="relu", strides=(2, 2)))
+model.add(Conv2D(48, (5, 5), data_format="channels_last", activation="relu", strides=(2, 2)))
+model.add(Conv2D(64, (5, 5), data_format="channels_last", activation="relu"))
+model.add(Conv2D(64, (5, 5), data_format="channels_last", activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
+
+# model.add(Convolution2D(6, 5, 5, activation="relu"))
+# model.add(MaxPooling2D())
+# model.add(Convolution2D(6, 5, 5, activation="relu"))
+# model.add(MaxPooling2D())
+# model.add(Flatten())
+# model.add(Dense(120))
+# model.add(Dense(84))
+# model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+
+# history_object = model.fit_generator(train_generator, samples_per_epoch =
+#     len(train_samples), validation_data = 
+#     validation_generator,
+#     nb_val_samples = len(validation_samples), 
+#     nb_epoch=5, verbose=1)
+
+# ### print the keys contained in the history object
+# print(history_object.history.keys())
+
+# ### plot the training and validation loss for each epoch
+# plt.plot(history_object.history['loss'])
+# plt.plot(history_object.history['val_loss'])
+# plt.title('model mean squared error loss')
+# plt.ylabel('mean squared error loss')
+# plt.xlabel('epoch')
+# plt.legend(['training set', 'validation set'], loc='upper right')
+# plt.show()
 
 model.save('model.h5')
